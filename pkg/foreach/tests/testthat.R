0f824afa4be8e19dd594a13c62f5622916638201
Hong Ooi <hongooi@microsoft.com>
2020-02-19 10:47:38 +1100

    new repo

--- pkg/foreach/tests/testthat.R
+++
@@ -1,9 +0,0 @@
-library(testthat)
-library(foreach)
-
-
-Sys.setenv(FOREACH_BACKEND="SEQ")
-test_check("foreach")
-
-Sys.setenv(FOREACH_BACKEND="PAR")
-test_check("foreach")
--- pkg/foreach/tests/testthat/test_packages.R
+++
@@ -1,7 +0,0 @@
-context("Packages")
-
-test_that("Package loading works", {
-  d <- foreach(1:10, .packages='splines', .combine='c') %dopar%
-               xyVector(c(1:3), c(4:6))[[1]]
-  expect_true(all(c(1:3) == d))
-})
